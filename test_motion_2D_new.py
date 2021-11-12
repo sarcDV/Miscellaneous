0f4f9b71c18586484e61679e8fcbfbf6937587e8
sarcDV <alex.sciarra@gmail.com>
2021-11-12 13:53:29 +0100

    Add files via upload

--- test_motion_2D_new.py
+++ test_motion_2D_new.py
@@ -28,6 +28,12 @@ def pad3D(invol, max1d, max2d, max3d):
     return aa
 
 def pad2D(invol, max1d, max2d):
+    if (invol.shape[0] % 2) != 0:  # if shape[0] is dispari  (odd)
+        invol = np.concatenate((invol, np.zeros((1,invol.shape[1]))), axis=0)
+        
+    if (invol.shape[1] % 2) != 0:  # if shape[1] is dispari  (odd)
+        invol = np.concatenate((invol, np.zeros((invol.shape[0],1))), axis=1)
+
     aa = np.pad(invol, 
             (((max1d-invol.shape[0])//2, (max1d-invol.shape[0])//2),
              ((max2d-invol.shape[1])//2, (max2d-invol.shape[1])//2)), 
@@ -65,7 +71,7 @@ def generate_motion_2d(img):
                                                      size=int(img.shape[1]//n_+1), dtype=int)))
         random_rots = np.random.randint(-4,4,len(portion))
         
-        for kk in range(0, len(portion)):
+        for kk in range(0, len(portion)-1):
             img_aux = ndimage.rotate(img, random_rots[kk], reshape=False)
             img_h = np.fft.fft2(img_aux)
             aux[:,portion[kk]]=img_h[:,portion[kk]]        
@@ -96,34 +102,42 @@ def generate_motion_2d(img):
 
 def corrupt_recursively(test):
     rnd_orient = np.random.randint(0,3,1)[0]
+    # print(rnd_orient)
     if rnd_orient == 0:
         rndslice_ = np.random.randint(low=int(test.shape[1]//2)-int(test.shape[1]//3), 
-                                    high=int(test.shape[1]//2)+int(test.shape[1]//3)-4, 
+                                    high=int(test.shape[1]//2)+int(test.shape[1]//3), 
                                     size=1)
+        
         img = (test[:,rndslice_[0],:])
         img = (img-img.min())/(img.max()-img.min())
         cor = generate_motion_2d(img)
     elif rnd_orient == 1:
         rndslice_ = np.random.randint(low=int(test.shape[2]//2)-int(test.shape[2]//3), 
-                                    high=int(test.shape[2]//2)+int(test.shape[2]//3)-4, 
+                                    high=int(test.shape[2]//2)+int(test.shape[2]//3), 
                                     size=1)
+                                   
         img = np.rot90(test[:,:,rndslice_[0]]) 
         img = (img-img.min())/(img.max()-img.min())
         cor = generate_motion_2d(img)
     else:
         rndslice_ = np.random.randint(low=int(test.shape[0]//2)-int(test.shape[0]//3), 
-                                    high=int(test.shape[0]//2)+int(test.shape[0]//3)-4, 
+                                    high=int(test.shape[0]//2)+int(test.shape[0]//3), 
                                     size=1)
+                                    
         img = np.flipud(test[rndslice_[0],:,:])
         img = (img-img.min())/(img.max()-img.min())
         cor = generate_motion_2d(img)
 
     return img, cor, rndslice_, rnd_orient
 
-test = nib.load('T1_sag_1_o.nii.gz').get_fdata()
+# test = nib.load('./niifiles/T1_sag_1_o.nii.gz').get_fdata()
+test = nib.load('./niifiles/IXI002-Guys-0828-T1.nii.gz').get_fdata()
+# img, cor, slice, orient = corrupt_recursively(test)
+
+
 
 ssimarray_ = np.zeros((100))
-niiinit_ = np.zeros((256,512,len(ssimarray_)))
+niiinit_ = np.zeros((512,256,len(ssimarray_)))
 for ii in range(0, len(ssimarray_)):
     img, cor, slice, orient = corrupt_recursively(test)
     print("Iteration# "+str(ii+1)+", SSIM="+str(ssim(img, cor, data_range=1))+\
@@ -131,13 +145,12 @@ for ii in range(0, len(ssimarray_)):
     ssimarray_[ii] = ssim(img, cor, data_range=1)
     img = pad2D(img, 256, 256)
     cor = pad2D(cor, 256, 256)
-    comb = np.concatenate((img, cor), axis=1)
+    comb = np.concatenate((np.rot90(img), np.rot90(cor)), axis=0)
     niiinit_[:,:,ii] = comb 
-    
-    
-plt.figure()
-plt.plot(ssimarray_)
-plt.show()
 
 c_= nib.Nifti1Image(np.abs(niiinit_), None)
-nib.save(c_, 'test_new_2D.nii.gz')
+nib.save(c_, 'test_new_2D_v06.nii.gz')
+plt.figure()
+plt.plot(ssimarray_,'*-')
+plt.title('SSIM')
+plt.show()
